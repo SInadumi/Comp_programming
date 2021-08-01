1b49a42c405551a4517bbf3c1c4c8560f4dbc42b
SInadumi <inazumi19@icloud.com>
2021-08-02 00:01:09 +0900

    ABC  210 A-C

---
+++ ABC/210/c.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+typedef pair<ll,ll> P;
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define EFOR(i,a,b) for(int i=(a);i<=(b);++i)
+#define REP(i,n) FOR(i,0,n)
+#define FORE(i,I) for(const auto &(i):(I))
+#define ALL(x) (x).begin(),(x).end()
+#define SIZE(x) ll(x.size())
+#define INF32 2147483647 
+#define INF64 9223372036854775807
+#define MOD 1000000007
+#define PI acos(-1)
+#define endl '\n'
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+// 尺取り法
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    ll n,k; cin >> n >> k;
+    vector<ll> c(n);
+    map<ll,ll> bucket;
+    REP(i,n) cin >> c[i];
+    ll ans = 1;
+    REP(i,n){
+        auto it = bucket.find(c[i]);
+        if(it != bucket.end()){
+            bucket[c[i]] ++;
+        }else{
+            bucket[c[i]] = 1;
+        }
+        if(i >= k){
+            it = bucket.find(c[i-k]);
+            bucket[c[i-k]] --;
+            if(bucket[c[i-k]] == 0) bucket.erase(it);
+        }
+        auto cnt = bucket.size();
+        ans = max(ans , (ll)cnt);
+    }
+    cout << ans << endl;
+    return 0;
+}
